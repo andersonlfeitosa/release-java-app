c770bce21aff06f419b809080aff851bd8eb54b1
Anderson Lobo Feitosa <andersonlf@gmail.com>
2019-05-21 14:27:25 -0300

    first commit

---
+++ src/main/java/com/andersonlfeitosa/release/Application.java
@@ -0,0 +1,21 @@
+package com.andersonlfeitosa.release;
+
+/**
+ * Application Class
+ *
+ * @author Anderson Lobo Feitosa
+ * @version 1.0.0
+ * @since 2019-05-19
+ */
+public class Application {
+
+  /**
+   * The main method.
+   *
+   * @param args The parameters.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+
+}
---
+++ src/test/java/com/andersonlfeitosa/release/TestApplication.java
@@ -0,0 +1,24 @@
+package com.andersonlfeitosa.release;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test application class.
+ *
+ * @author Anderson Lobo Feitosa
+ * @version 1.0.0
+ * @since 2019-05-19
+ */
+public class TestApplication {
+
+  /**
+   * The test main method.
+   */
+  @Test
+  public void test_main() {
+    assertEquals(1, 1);
+  }
+
+}
