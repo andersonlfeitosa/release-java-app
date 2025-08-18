940731a12e1c16cbd2d540d6a9122bf04268368e
Anderson Lobo Feitosa <andersonlf@gmail.com>
2025-08-18 00:03:57 -0500

    make it better

--- release-prepare.sh
+++ release-prepare.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-mvn -B release:prepare \
+show_error() {
+  echo "Usage: $(basename "$0") VERSION NEXT_VERSION"
+  echo "Final version will be: VERSION: 1.0.VERSION and 1.0.NEXT_VERSION-SNAPSHOT"
+}
+
+if [[ $# == 2 ]]
+then
+  mvn -B release:prepare \
           -DreleaseVersion="1.0.$1" \
           -DdevelopmentVersion="1.0.$2-SNAPSHOT" \
           -Dgoals=install \
           -Darguments="-Dmaven.test.skip=true -Dmaven.javadoc.skip=true"
+else
+  show_error
+fi
