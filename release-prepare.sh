befbd0ccf63eee1ad3602d1e7fd9c5353ffe90c5
Anderson Lobo Feitosa <andersonlf@gmail.com>
2025-08-18 01:14:35 -0500

    make it better

--- release-prepare.sh
+++ release-prepare.sh
@@ -8,8 +8,6 @@ show_error() {
 if [[ $# == 2 ]]
 then
   mvn -B release:prepare \
-          -DreleaseVersion="1.0.$1" \
-          -DdevelopmentVersion="1.0.$2-SNAPSHOT" \
           -Dgoals=install \
           -Darguments="-Dmaven.test.skip=true -Dmaven.javadoc.skip=true"
 else
